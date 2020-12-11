c6d8472998d3456f9e70204f2218f4d2b15d3b75
john <john@jpatmore.plus.com>
2020-12-11 16:35:39 +0000

    test code

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ window.minsize(width=300, height=200)
 # Label
 my_label = tkinter.Label(text="My label", font=("Arial", 16, "bold"))  # Create the label
 my_label.pack()  # Specify layout on the screen (default: top center)
-"""In contrast to the more cumbersome placer,the packer takes 
+"""
+In contrast to the more cumbersome placer,the packer takes 
 qualitative relationship specification - above, to the left of, 
 filling, etc - and works everything out to determine the exact 
-placement coordinates for you."""
+placement coordinates for you. The size of any master widget 
+is determined by the size of the “slave widgets” inside it.
+https://docs.python.org/3.9/library/tkinter.html#the-packer
+"""
 
 
 # Just like Turtle Graphics, this must be the last line of the program
